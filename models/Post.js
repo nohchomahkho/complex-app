94d6bd8011367eef284b39ac1d10b24d72730872
nohchomahkho <159694273+nohchomahkho@users.noreply.github.com>
2024-03-18 19:15:57 +0100

    Update Post.js

--- models/Post.js
+++ models/Post.js
@@ -1,20 +1,16 @@
-const postsCollection = require("../db").db().collection("posts")
-const ObjectId = require("mongodb").ObjectId
-const User = require("./User")
+const postsCollection = require('../db').db().collection("posts")
+const ObjectId = require('mongodb').ObjectId
+const User = require('./User')
 
-let Post = function (data, userid) {
+let Post = function(data, userid) {
   this.data = data
   this.errors = []
   this.userid = userid
 }
 
-Post.prototype.cleanUp = function () {
-  if (typeof this.data.title != "string") {
-    this.data.title = ""
-  }
-  if (typeof this.data.body != "string") {
-    this.data.body = ""
-  }
+Post.prototype.cleanUp = function() {
+  if (typeof(this.data.title) != "string") {this.data.title = ""}
+  if (typeof(this.data.body) != "string") {this.data.body = ""}
 
   // get rid of any bogus properties
   this.data = {
@@ -25,44 +21,32 @@ Post.prototype.cleanUp = function () {
   }
 }
 
-Post.prototype.validate = function () {
-  if (this.data.title == "") {
-    this.errors.push("You must provide a title.")
-  }
-  if (this.data.body == "") {
-    this.errors.push("You must provide post content.")
-  }
+Post.prototype.validate = function() {
+  if (this.data.title == "") {this.errors.push("You must provide a title.")}
+  if (this.data.body == "") {this.errors.push("You must provide post content.")}
 }
 
-Post.prototype.create = function () {
+Post.prototype.create = function() {
   return new Promise((resolve, reject) => {
     this.cleanUp()
     this.validate()
     if (!this.errors.length) {
       // save post into database
-      postsCollection
-        .insertOne(this.data)
-        .then(() => {
-          resolve()
-        })
-        .catch(() => {
-          this.errors.push("Please try again later.")
-          reject(this.errors)
-        })
+      postsCollection.insertOne(this.data).then(() => {
+        resolve()
+      }).catch(() => {
+        this.errors.push("Please try again later.")
+        reject(this.errors)
+      })
     } else {
       reject(this.errors)
     }
   })
 }
 
-Post.findSingleById = function(id){
-  return new Promise(async function(resolve, reject){
-    if(typeof(id) != "string" || !ObjectId.isValid(id)){
-      reject()
-      return
-    }
-    let posts = await postsCollection.aggregate([
-      {$match: {_id: new ObjectId(id)}},
+Post.reusablePostQuery = function(uniqueOperations) {
+  return new Promise(async function(resolve, reject) {
+    let aggOperations = uniqueOperations.concat([
       {$lookup: {from: "users", localField: "author", foreignField: "_id", as: "authorDocument"}},
       {$project: {
         title: 1,
@@ -70,24 +54,49 @@ Post.findSingleById = function(id){
         createdDate: 1,
         author: {$arrayElemAt: ["$authorDocument", 0]}
       }}
-    ]).toArray()
+    ])
+
+    let posts = await postsCollection.aggregate(aggOperations).toArray()
 
     // clean up author property in each post object
-    posts = posts.map(function(post){
+    posts = posts.map(function(post) {
       post.author = {
         username: post.author.username,
-        avatar: new User (post.author, true).avatar
+        avatar: new User(post.author, true).avatar
       }
+
       return post
     })
 
-    if(posts.length){
+    resolve(posts)
+  })
+}
+
+Post.findSingleById = function(id) {
+  return new Promise(async function(resolve, reject) {
+    if (typeof(id) != "string" || !ObjectID.isValid(id)) {
+      reject()
+      return
+    }
+    
+    let posts = await Post.reusablePostQuery([
+      {$match: {_id: new ObjectId(id)}}
+    ])
+
+    if (posts.length) {
       console.log(posts[0])
       resolve(posts[0])
-    }else{
+    } else {
       reject()
     }
   })
+}
+
+Post.findByAuthorId = function(authorId) {
+  return Post.reusablePostQuery([
+    {$match: {author: authorId}},
+    {$sort: {createdDate: -1}}
+  ])
 }
 
 module.exports = Post
