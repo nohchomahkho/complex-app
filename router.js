36bda2c734118e49ca5f2fc44a3f059cb860cefb
nohchomahkho <159694273+nohchomahkho@users.noreply.github.com>
2024-03-18 18:30:31 +0100

    Update router.js

--- router.js
+++ router.js
@@ -2,7 +2,6 @@ const express = require ('express')
 const router = express.Router()
 const userController = require ('./controllers/userController')
 const postController = require ('./controllers/postController')
-const
 
 // user related routes
 router.get('/', userController.home)
